fb77878598589a149fd9049621ff50c5502c7991
Justin Konrad <jkonrad@wredegmbh.de>
2024-12-03 09:58:40 +0100

    Login-Site

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require('ejs');
 // app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
